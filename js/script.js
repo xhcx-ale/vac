31374d35aaa816693f314163a744838ba898dda7
xhcx-ale <alexzpta8@gmail.com>
2023-12-22 03:55:45 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+var theme = localStorage.getItem('theme');
+$('body').attr('data-bs-theme', theme);
+$('#light').on('click', function() {
+         $('body').attr('data-bs-theme', 'light');
+         localStorage.setItem('theme', 'light');
+});
+
+$('#dark').on('click', function() {
+         $('body').attr('data-bs-theme', 'dark');
+         localStorage.setItem('theme', 'dark');
+});
+var list = localStorage.getItem('list');
+if(list == 1){
+    var vacList = 'vac1';
+} else if(list == 2){
+    var vacList = 'vac2';
+}
+function tbLoad(){
+    fetch(vacList + '.txt')
+    .then(respone => respone.text())
+    .then(content => {
+        elementos = content.split('\n').filter(x => x.indexOf('#') !== -1);
+        nombres = content.split('\n').filter(x => x.indexOf('=') !== -1);
+        horarios = content.split('\n').filter(x => x.indexOf('+') !== -1);
+        dias = content.split('\n').filter(x => x.indexOf('!') !== -1);
+        descs = content.split('\n').filter(x => x.indexOf('*') !== -1);
+        lugares = content.split('\n').filter(x => x.indexOf('?') !== -1);
+        
+        elementos.forEach((elemento, index) => {
+            var trVac = '<tr><th id="index' + index + '" scope="row"></th><td id="place' + index + '"></td><td id="sal' + index + '"></td><td id="turno' + index + '"></td><td class="td-btn"><button class="btn-ver" id="btn-ver' + index + '">Ver</button></td></tr>';
+            $('tbody').append(trVac);
+            var tbTitle = document.createElement('p');
+            var tbLocat = document.createElement('p');
+            tbTitle.textContent = index + 1;
+            tbLocat.textContent = elemento.substring(1, elemento.length);
+            $('#index' + index).append(tbTitle);
+            $('#place' + index).append(tbLocat);
+
+        });
+        salarios = content.split('\n').filter(x => x.indexOf('$') !== -1);
+        salarios.forEach((salario, index) => {
+            var tbSala = document.createElement('p');
+            tbSala.textContent = salario.substring(1, salario.length);
+            $('#sal' + index).append(tbSala);
+        });
+
+        
+        turnos = content.split('\n').filter(x => x.indexOf('/') !== -1);
+        turnos.forEach((turno, index) => {
+            var tbTurno = document.createElement('p');
+            tbTurno.textContent = turno.substring(1, turno.length);
+            $('#turno' + index).append(tbTurno);
+            $('#btn-ver' + index).on('click', function(){
+                localStorage.setItem('index', index + 1);
+                var Ind = index;
+                var shNam = nombres.find((nombre, index) => index === Ind);
+                localStorage.setItem('nombre', shNam.substring(1, shNam.length));
+               
+                var shTrn = turnos.find((turno, index) => index === Ind);
+                localStorage.setItem('turno', shTrn.substring(1, shTrn.length));
+              
+                var shHr = horarios.find((horario, index) => index === Ind);
+                localStorage.setItem('horario', shHr.substring(1, shHr.length));
+                 
+                var shDia = dias.find((dia, index) => index === Ind);
+                localStorage.setItem('dia', shDia.substring(1, shDia.length));
+                
+                var shSal = salarios.find((salario, index) => index === Ind);
+                localStorage.setItem('salario', shSal.substring(1, shSal.length));
+
+                var shDesc = descs.find((desc, index) => index === Ind);
+                localStorage.setItem('desc', shDesc.substring(1, shDesc.length));
+                var shDir = lugares.find((lugar, index) => index === Ind);
+                localStorage.setItem('direccion', shDir.substring(1, shDir.length));
+                location.href = 'detalles-vacante.html';
+            });
+        });
+    });
+};
+
+$(document).ready(function(){
+    tbLoad();
+});
+$('#btn-vac1').on('click', function(){
+    localStorage.setItem('list', 1);
+    location.href="vacantes.html";
+});
+$('#btn-vac2').on('click', function(){
+    localStorage.setItem('list', 2);
+    location.href="vacantes.html";
+});
---
+++ js/script1.js
@@ -0,0 +1,132 @@
+var chosInd = localStorage.getItem('index'),
+    nombre = localStorage.getItem('nombre'),
+    turno = localStorage.getItem('turno'),
+    horario = localStorage.getItem('horario'),
+    dia = localStorage.getItem('dia'),
+    salario = localStorage.getItem('salario'),
+    descripcion = localStorage.getItem('desc'),
+    direccion = localStorage.getItem('direccion');
+
+$('#nmVac').text(nombre);
+$('#vacTur').text(turno);
+$('#vacHr').text(horario);
+$('#vacDia').text(dia);
+$('#vacSal').text(salario);
+$('#vacDesc').text(descripcion);
+$('#vacDir').text(direccion);
+
+var theme = localStorage.getItem('theme');
+$('body').attr('data-bs-theme', theme);
+
+$('#light').on('click', function() {
+         $('body').attr('data-bs-theme', 'light');
+         localStorage.setItem('theme', 'light');
+});
+
+$('#dark').on('click', function() {
+         $('body').attr('data-bs-theme', 'dark');
+         localStorage.setItem('theme', 'dark');
+});
+
+$('#wsp').on('click', function() {
+  var msg = 'Hola, me interesa la vacante ' + nombre + ', turno ' + turno + ', en ' + direccion + '.';
+  location.href = "https://wa.me/528131715194/?text=" + msg;
+});
+
+$(document).ready(function() {
+    var divMap = redrawMap();
+    $('#mapa').append(divMap);
+});
+
+var title = 'Test';
+var street = direccion; 
+var coord = ''; 
+var mType = '';
+var mapWidth = 100 + '%'; var mapHeight = 400;
+var mapZoom = 14;
+
+function redrawMap() { var iframeUrl = generateMapUrl(); 
+    var finalCode = getFinalWidgetCode(); 
+    var finc = '<div style="width: 100%">' + finalCode + '</div>'; 
+    return finc;
+}
+
+function getFinalWidgetCode() { var $iframe = getIFrame(); $iframe.append(getLinkBaitScript()); var finalWidgetCode = $iframe.wrap('<div />').parent().html(); return finalWidgetCode;
+
+}
+
+function getIFrame() { var $iframe = $('<iframe></iframe>'); $iframe.attr("width", mapWidth); $iframe.attr("height", mapHeight); $iframe.attr("frameborder", "0"); $iframe.attr("scrolling", "no"); $iframe.attr("marginheight", 0); $iframe.attr("marginwidth", 0); $iframe.attr("src", generateMapUrl()); 
+return $iframe;
+
+}
+
+function getLinkBaitScript() { var z = 5;
+
+var texte = new Array(z);
+texte[0] = "Sat Navs";
+
+texte[1] = "GPS devices";
+
+texte [2] = "Car GPS";
+
+texte[3] = "Car Navigation Systems";
+
+texte[4] = "GPS car tracker";
+
+var jetzt = new Date();
+
+z = (jetzt.getSeconds()) % z;
+
+var linkText = texte[z];
+
+var linkBaitCode = '<a href="https://www.gps.ie/car-satnav-gps/">' + linkText + '</ a>';
+
+return linkBaitCode; }
+
+function generateMapUrl() {
+
+var googleMapsHostUrl = "https://maps.google.com/maps";
+
+var query = "";
+
+if (street !== '') {
+
+query = street + "+(" + title + ")";
+
+} else {
+
+query = coord + "+(" + title + ")"; }
+
+var urlParams = {
+
+width: mapWidth,
+
+height: mapHeight,
+
+hl: "es",
+
+q: query,
+
+t: mType,
+
+z: mapZoom,
+
+ie: "UTF8",
+
+iwloc: "B",
+
+output: "embed"
+
+};
+
+var paramString = "";
+
+for (var key in urlParams) {
+
+paramString += key + "=" +
+
+urlParams[key] + "&";
+
+}
+var fullUrl = googleMapsHostUrl + "?" + encodeURI(paramString.replace(/.$/,"")); return fullUrl; } 
+redrawMap();
